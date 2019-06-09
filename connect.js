a90d7ad34443e9575507d8634d7130d7c4699733
Kendy Nguyen <kendyisnguyen@gmail.com>
2019-06-09 14:21:52 -0700

    add then and catch to mongoDB connection

--- connect.js
+++ connect.js
@@ -4,5 +4,8 @@ const mongoose = require("mongoose");
 // Connect to the db
 module.exports = connect = url => {
   url = process.env.dbUrl;
-  return mongoose.connect(url, { useNewUrlParser: true });
+  return mongoose
+    .connect(url, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB connected ... "))
+    .catch(err => console.log(err));
 };
