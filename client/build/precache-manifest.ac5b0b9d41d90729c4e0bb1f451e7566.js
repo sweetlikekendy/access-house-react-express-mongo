583fef4f680460824848dcb6ef851ba8c51fe8d6
Kendy Nguyen <kendyisnguyen@gmail.com>
2020-03-24 15:11:41 -0700

    remove build from gitignore from cra

---
+++ client/build/precache-manifest.ac5b0b9d41d90729c4e0bb1f451e7566.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114cbe362ee3583cb04c1a9ae9810856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bf0234746e181a2c531",
+    "url": "/static/js/2.76cf13ce.chunk.js"
+  },
+  {
+    "revision": "fd5a9a2466660d48a08d6025fc8ce9fb",
+    "url": "/static/js/2.76cf13ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545e4b4730204d33f4ca",
+    "url": "/static/js/main.2e9e593f.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
