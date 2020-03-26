380280f4819ab75d4ee90088e7f214006ee38973
Kendy Nguyen <kendyisnguyen@gmail.com>
2020-03-25 17:10:18 -0700

    add dirname to set static folder in server

--- server/server.js
+++ server/server.js
@@ -17,10 +17,10 @@ app.use("/api/homes", homeRoute);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(__dirname, "../client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
